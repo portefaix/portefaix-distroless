9cec9b104fa35c36c025a291934b5ccd64dddb4f
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2023-08-17 18:41:12 +0200

    Add: license header
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- cicd/build.sh
+++ cicd/build.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 set -feu -o pipefail
 
 reset_color="\\e[0m"
@@ -22,11 +38,11 @@ test -f melange.rsa || docker run --rm -it --privileged \
 
 echo_info "[portefaix-distroless] Build the APK"
 docker run --privileged --rm -v "$(pwd):/work" \
-  cgr.dev/chainguard/melange build melange.yaml --arch "${ARCH}" --signing-key melange.rsa
+    cgr.dev/chainguard/melange build melange.yaml --arch "${ARCH}" --signing-key melange.rsa
 
 echo_info "[portefaix-distroless] Building the Container Image"
 docker run --rm -v "$(pwd):/work" \
     cgr.dev/chainguard/apko build apko.yaml portefaix-cicd:test portefaix-cicd.tar --arch "${ARCH}" -k melange.rsa.pub
-    
+
 echo_info "[portefaix-distroless] Load image"
-docker load < portefaix-cicd.tar
+docker load <portefaix-cicd.tar
--- cicd/entrypoint.sh
+++ cicd/entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copyright (C) Nicolas Lamirault <nicolas.lamirault@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 [ -n "$1" ] && exec "$@"
 
 cat <<EOF
@@ -9,7 +25,7 @@ cat <<EOF
  _____) )__   ____| |_  ____| |__ ____ _ _   _         helm: ${HELM_VERSION}
 |  ____/ _ \ / ___)  _)/ _  )  __) _  | ( \ / )        kustomize: ${KUSTOMIZE_VERSION}
 | |   | |_| | |   | |_( (/ /| | ( ( | | |) X (
-|_|    \___/|_|    \___)____)_|  \_||_|_(_/ \_)  
+|_|    \___/|_|    \___)____)_|  \_||_|_(_/ \_)
 
 Home: https://github.com/portefaix/portefaix-distroless
 
